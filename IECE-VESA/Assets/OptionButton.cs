6f16653a55bf60cfbfcdb1b047f6aafe309293a8
Dylan Updegrave <dylanupde@gmail.com>
2020-10-08 17:46:17 -0600

    Initial commit

---
+++ IECE-VESA/Assets/OptionButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButton : MonoBehaviour
+{
+    public Option option;
+    
+
+
+    public void OnClick()
+    {
+        GameManager gameManager = GameManager.Instance;
+
+        if (option.pointsWorth == -1)
+        {
+            gameManager.FailThem();
+        }
+        else
+        {
+            gameManager.AddPoints(option.pointsWorth);
+            gameManager.LoadNextQuestion(option.letter.ToString());
+        }
+
+        gameManager.DisplayComment(option.comment);
+    }
+}
